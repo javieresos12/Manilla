9cd0165d22a02a987333c260405ba24a100587e7
javier <javieresos12@gmail.com>
2018-08-30 19:35:31 -0500

    aplicacion funcional definitivamente

--- app/src/main/java/com/example/android/primertaller/Principal.java
+++ app/src/main/java/com/example/android/primertaller/Principal.java
@@ -91,14 +91,14 @@ public class Principal extends AppCompatActivity {
         if (cantidad.getText().toString().isEmpty()){
             cantidad.setError(recursos.getString(R.string.Error1));
             cantidad.requestFocus();
-            res.setText("Resultado");
+            res.setText(recursos.getString(R.string.Error7));
             return false;
         }
         canti=Integer.parseInt(cantidad.getText().toString());
         if (canti<=0){
             cantidad.setError(recursos.getString(R.string.Error6));
             cantidad.requestFocus();
-            res.setText("Resultado");
+            res.setText(recursos.getString(R.string.Error7));
             return false;
 
         }
